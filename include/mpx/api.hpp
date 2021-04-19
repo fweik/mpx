d05646d3377c432a7ad26de00f0d5d0f26a9cd45
Florian Weik <fweik@icp.uni-stuttgart.de>
2021-04-19 21:18:13 +0200

    patching prototype

--- include/mpx/api.hpp
+++ include/mpx/api.hpp
@@ -7,6 +7,7 @@
 
 #include <any>
 #include <cstdint>
+#include <unordered_map>
 #include <vector>
 
 namespace mpx::api {
@@ -26,6 +27,34 @@ struct null {
         *std::get<1>(std::tie(args...)) = 1;
       }
     }
+  }
+};
+
+template <class Api = null> struct patched {
+  using void_fun = void (*)(void);
+
+  template <class... Args, class F>
+  static void patch(int (*orig)(Args...), F subst) {
+    int (*sp)(Args...) = subst;
+
+    patches()[reinterpret_cast<void_fun>(orig)] =
+        reinterpret_cast<void_fun>(sp);
+  }
+
+  static auto &patches() {
+    static std::unordered_map<void_fun, void_fun> m_patches;
+
+    return m_patches;
+  }
+
+  template <class... Args> static void call(int (*fp)(Args...), Args... args) {
+    auto it = patches().find(reinterpret_cast<void_fun>(fp));
+
+    if (it != patches().end()) {
+      (*reinterpret_cast<decltype(fp)>(it->second))(args...);
+    }
+
+    Api::call(fp, args...);
   }
 };
 
--- tests/communicator.test.cpp
+++ tests/communicator.test.cpp
@@ -5,14 +5,25 @@
 
 TEST_CASE("rank") {
   mpx::communicator comm{};
+  mpx::api::patched<>::patch(&MPI_Comm_rank,
+                             [](mpx::communicator, int *rank) -> int {
+                               *rank = 0;
+                               return MPI_SUCCESS;
+                             });
 
-  auto const r = mpx::rank<mpx::api::null>(comm);
+  auto const r = mpx::rank<mpx::api::patched<>>(comm);
   CHECK(static_cast<int>(r) == 0);
 }
 
 TEST_CASE("size") {
   mpx::communicator comm{};
 
-  auto const r = mpx::size<mpx::api::null>(comm);
+  mpx::api::patched<>::patch(&MPI_Comm_size,
+                             [](mpx::communicator, int *size) -> int {
+                               *size = 1;
+                               return MPI_SUCCESS;
+                             });
+
+  auto const r = mpx::size<mpx::api::patched<>>(comm);
   CHECK(r == 1);
 }
