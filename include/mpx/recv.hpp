27c2fe4f28aa7fed23a169679539daa2a3f4bf12
Florian Weik <fweik@icp.uni-stuttgart.de>
2021-04-26 22:10:26 +0200

    Addded api to send

--- include/mpx/recv.hpp
+++ include/mpx/recv.hpp
@@ -1,20 +1,18 @@
 #ifndef MPX_RECV_HPP
 #define MPX_RECV_HPP
 
+#include <mpx/api.hpp>
 #include <mpx/communicator.hpp>
 #include <mpx/data_range.hpp>
-#include <mpx/request.hpp>
-
-#include <mpi.h>
 
 namespace mpx {
-template <class T>
+template <class T, class Api = default_api>
 void recv(communicator const &comm, Rank to, Tag tag, T &data) {
   auto const data_range = data_range_for<T>{}(data);
 
-  check_error_code(MPI_recv(data_range.data, data_range.type.count,
-                            data_range.type.type, static_cast<int>(to),
-                            static_cast<int>(tag), comm));
+  Api::eval(&MPI_Recv, data_range.data, data_range.type.count,
+            data_range.type.type, static_cast<int>(to), static_cast<int>(tag),
+            comm);
 }
 } // namespace mpx
 
--- tests/send.test.cpp
+++ tests/send.test.cpp
@@ -1,3 +1,17 @@
 #include <doctest/doctest.h>
 
 #include <mpx/send.hpp>
+
+TEST_CASE("send") {
+  using api = mpx::api::log<mpx::api::null>;
+
+  mpx::communicator comm;
+  mpx::Rank to{5};
+  mpx::Tag tag{6};
+  const int data = 7;
+
+  auto const &log = api::scope_log();
+  mpx::send<api>(comm, to, tag, data);
+
+  call_compare(log.front(), &MPI_Send, comm, to, tag, data);
+}
