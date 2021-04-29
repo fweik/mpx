87cb9fd2dcc3ccd4a3fd5a4c32ff244a9e18537e
Florian Weik <fweik@icp.uni-stuttgart.de>
2021-04-29 22:08:31 +0200

    test for recv

--- include/mpx/recv.hpp
+++ include/mpx/recv.hpp
@@ -6,13 +6,13 @@
 #include <mpx/data_range.hpp>
 
 namespace mpx {
-template <class T, class Api = default_api>
+template <class Api = default_api, class T>
 void recv(communicator const &comm, Rank to, Tag tag, T &data) {
-  auto const data_range = data_range_for<T>{}(data);
+  auto const data_range = data_range_for<T>::get(data);
 
-  Api::invoke(&MPI_Recv, data_range.data, data_range.type.count,
-            data_range.type.type, static_cast<int>(to), static_cast<int>(tag),
-            comm);
+  Api::invoke(&MPI_Recv, data_range.data, data_range.count, data_range.type,
+              static_cast<int>(to), static_cast<int>(tag), comm,
+              MPI_STATUSES_IGNORE);
 }
 } // namespace mpx
 
